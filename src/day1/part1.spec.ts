43fa7a7fa295c5332fd04d9e5926d9951ce58112
Jonathan Darrer <jdarrer@findmypast.com>
2022-12-01 12:32:03 +0000

    1-1 ✨ Add day 1 part 1

--- src/day1/part1.spec.ts
+++ src/day1/part1.spec.ts
@@ -1,14 +1,23 @@
-import part1, { somethingElse } from './part1';
+import part1 from "./part1";
 
-describe('day1/part1', () => {
-  it('should work out the result', () => {
-    const input = [];
-    expect(part1(input)).toEqual(0);
-  });
-});
-describe('day1/somethingElse', () => {
-  it('should work out something', () => {
-    const input = 0;
-    expect(somethingElse(input)).toEqual(0);
+describe("day1/part1", () => {
+  it("should work out the most amount of calories being carried by any elf", () => {
+    const input = [
+      1000,
+      2000,
+      3000,
+      null,
+      4000,
+      null,
+      5000,
+      6000,
+      null,
+      7000,
+      8000,
+      9000,
+      null,
+      10000,
+    ];
+    expect(part1(input)).toEqual(24_000);
   });
 });
--- src/day1/part1.ts
+++ src/day1/part1.ts
@@ -1,6 +1,16 @@
-const part1 = (input: Array<string>): number => 0;
+const part1 = (input: Array<number>): number => {
+  const elfCalories: Array<number> = [0];
 
-const somethingElse = (arg: number): number => 0;
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === null) {
+      elfCalories.push(0);
+      continue;
+    }
+    elfCalories[elfCalories.length - 1] =
+      elfCalories[elfCalories.length - 1] + input[i];
+  }
+
+  return elfCalories.reduce((prev, curr) => (curr > prev ? curr : prev), 0);
+};
 
 export default part1;
-export { somethingElse };
