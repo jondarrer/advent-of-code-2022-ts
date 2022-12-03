31283a154ebd4da4378c57dc79718b2690c92510
Jonathan Darrer <jdarrer@findmypast.com>
2022-12-03 20:03:06 +0000

    2-2 ✨ Add day 2 part 2

--- src/day2/part2.spec.ts
+++ src/day2/part2.spec.ts
@@ -1,8 +1,42 @@
-import part2 from "./part2";
+import part2, { calculateResponseInRPS } from "./part2";
 
 describe("day2/part2", () => {
-  it("should work out the result", () => {
-    const input = [];
-    expect(part2(input)).toEqual(0);
+  it("should work out the total score provided in the strategy guide", () => {
+    const input = [
+      ["A", "Y"],
+      ["B", "X"],
+      ["C", "Z"],
+    ];
+    expect(part2(input)).toEqual(12);
+  });
+});
+
+describe("day2/calculateResponseInRPS", () => {
+  it("should work out the response in a game of rock, paper, scissors for R-X", () => {
+    expect(calculateResponseInRPS("R", "X")).toEqual("S");
+  });
+  it("should work out the score in a game of rock, paper, scissors for R-Y", () => {
+    expect(calculateResponseInRPS("R", "Y")).toEqual("R");
+  });
+  it("should work out the score in a game of rock, paper, scissors for R-Z", () => {
+    expect(calculateResponseInRPS("R", "Z")).toEqual("P");
+  });
+  it("should work out the score in a game of rock, paper, scissors for P-X", () => {
+    expect(calculateResponseInRPS("P", "X")).toEqual("R");
+  });
+  it("should work out the score in a game of rock, paper, scissors for P-Y", () => {
+    expect(calculateResponseInRPS("P", "Y")).toEqual("P");
+  });
+  it("should work out the score in a game of rock, paper, scissors for P-Z", () => {
+    expect(calculateResponseInRPS("P", "Z")).toEqual("S");
+  });
+  it("should work out the score in a game of rock, paper, scissors for S-X", () => {
+    expect(calculateResponseInRPS("S", "X")).toEqual("P");
+  });
+  it("should work out the score in a game of rock, paper, scissors for S-Y", () => {
+    expect(calculateResponseInRPS("S", "Y")).toEqual("S");
+  });
+  it("should work out the score in a game of rock, paper, scissors for S-Z", () => {
+    expect(calculateResponseInRPS("S", "Z")).toEqual("R");
   });
 });
--- src/day2/part2.ts
+++ src/day2/part2.ts
@@ -1,3 +1,47 @@
-const part2 = (input: Array<string>): number => 0;
+import { calculateScore } from "./part1";
+
+const part2 = (input: Array<Array<string>>): number => {
+  return input
+    .map(([opponent, response]) => {
+      // Convert suggested response
+      let opponentInRPS, responseInRPS;
+      switch (opponent) {
+        case "A":
+          opponentInRPS = "R";
+          break;
+        case "B":
+          opponentInRPS = "P";
+          break;
+        default:
+          opponentInRPS = "S";
+      }
+      responseInRPS = calculateResponseInRPS(opponentInRPS, response);
+      return calculateScore(opponentInRPS, responseInRPS);
+    })
+    .reduce((prev, curr) => prev + curr, 0);
+};
+
+const calculateResponseInRPS = (a: string, b: string): string => {
+  if (b === "Y") return a;
+  if (b === "X") {
+    switch (a) {
+      case "R":
+        return "S";
+      case "P":
+        return "R";
+      default:
+        return "P";
+    }
+  }
+  switch (a) {
+    case "R":
+      return "P";
+    case "P":
+      return "S";
+    default:
+      return "R";
+  }
+};
 
 export default part2;
+export { calculateResponseInRPS };
