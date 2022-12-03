0f023995f72dda0f451078e7a66a3a21980375cd
Jonathan Darrer <jonny@jondarrer.me.uk>
2022-12-03 22:29:58 +0000

    ✨ 3-2 Add day 3 part 2

--- src/day3/part2.ts
+++ src/day3/part2.ts
@@ -1,4 +1,23 @@
-const part2 = (input: Array<string>): number => 0;
+import { itemToPriority } from "./part1";
+
+const part2 = (input: Array<Array<string>>): number => {
+  let sumOfPriorities = 0;
+
+  for (let i = 0; i < input.length; i = i + 3) {
+    const rucksack1 = new Set(input[i]);
+    const rucksack2 = new Set(input[i + 1]);
+    const rucksack3 = new Set(input[i + 2]);
+
+    for (const item of rucksack1) {
+      if (rucksack2.has(item) && rucksack3.has(item)) {
+        sumOfPriorities += itemToPriority(item);
+        break;
+      }
+    }
+  }
+
+  return sumOfPriorities;
+};
 
 const somethingElse = (arg: number): number => 0;
 
