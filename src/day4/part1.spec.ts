e30c99265ad471efe0db5d764903c88abc62db06
Jonathan Darrer <jonny@jondarrer.me.uk>
2022-12-04 10:22:51 +0000

    4-1 ✨ Add day 4 part 1

--- src/day4/part1.spec.ts
+++ src/day4/part1.spec.ts
@@ -1,14 +1,53 @@
-import part1, { somethingElse } from './part1';
+import part1, { oneRangeContainsTheOther } from "./part1";
 
-describe('day4/part1', () => {
-  it('should work out the result', () => {
-    const input = [];
-    expect(part1(input)).toEqual(0);
+describe("day4/part1", () => {
+  it("should work out the result", () => {
+    const input = [
+      [
+        [2, 4],
+        [6, 8],
+      ],
+      [
+        [2, 3],
+        [4, 5],
+      ],
+      [
+        [5, 7],
+        [7, 9],
+      ],
+      [
+        [2, 8],
+        [3, 7],
+      ],
+      [
+        [6, 6],
+        [4, 6],
+      ],
+      [
+        [2, 6],
+        [4, 8],
+      ],
+    ];
+    expect(part1(input)).toEqual(2);
   });
 });
-describe('day4/somethingElse', () => {
-  it('should work out something', () => {
-    const input = 0;
-    expect(somethingElse(input)).toEqual(0);
+describe("day4/oneRangeContainsTheOther", () => {
+  it("should work out whether one range contains the other: 1", () => {
+    expect(oneRangeContainsTheOther([2, 4], [6, 8])).toEqual(false);
+  });
+  it("should work out whether one range contains the other: 2", () => {
+    expect(oneRangeContainsTheOther([2, 3], [4, 5])).toEqual(false);
+  });
+  it("should work out whether one range contains the other: 3", () => {
+    expect(oneRangeContainsTheOther([5, 7], [7, 9])).toEqual(false);
+  });
+  it("should work out whether one range contains the other: 4", () => {
+    expect(oneRangeContainsTheOther([2, 8], [3, 7])).toEqual(true);
+  });
+  it("should work out whether one range contains the other: 5", () => {
+    expect(oneRangeContainsTheOther([6, 6], [4, 6])).toEqual(true);
+  });
+  it("should work out whether one range contains the other: 6", () => {
+    expect(oneRangeContainsTheOther([2, 6], [4, 8])).toEqual(false);
   });
 });
--- src/day4/part1.ts
+++ src/day4/part1.ts
@@ -1,6 +1,23 @@
-const part1 = (input: Array<string>): number => 0;
+const part1 = (input: Array<Array<Array<number>>>): number => {
+  let pairedRangesFullyContained = 0;
 
-const somethingElse = (arg: number): number => 0;
+  for (let i = 0; i < input.length; i++) {
+    if (oneRangeContainsTheOther(input[i][0], input[i][1])) {
+      pairedRangesFullyContained++;
+    }
+  }
+
+  return pairedRangesFullyContained;
+};
+
+const oneRangeContainsTheOther = (
+  [aStart, aEnd]: Array<number>,
+  [bStart, bEnd]: Array<number>
+): boolean => {
+  return (
+    (aStart <= bStart && aEnd >= bEnd) || (bStart <= aStart && bEnd >= aEnd)
+  );
+};
 
 export default part1;
-export { somethingElse };
+export { oneRangeContainsTheOther };
