f1985fc394161ba5dcf6f674e059bb73a130d62b
Jonathan Darrer <jonny@jondarrer.me.uk>
2022-12-04 09:09:25 +0000

    4-0 🚧 Implement file reader for day 4

--- src/day4/file-read-handler.spec.ts
+++ src/day4/file-read-handler.spec.ts
@@ -1,12 +1,44 @@
-import fileReadHandler from './file-read-handler';
+import fileReadHandler from "./file-read-handler";
 
-describe('day4/file-read-handler', () => {
-  it('should read in strings and convert them to numbers', () => {
-    const lines = ['1721', '979', '366', '299', '675', '1456'];
+describe("day4/file-read-handler", () => {
+  it("should read in strings and convert them to numbers", () => {
+    const lines = [
+      "2-4,6-8",
+      "2-3,4-5",
+      "5-7,7-9",
+      "2-8,3-7",
+      "6-6,4-6",
+      "2-6,4-8",
+    ];
     const accumulator = [];
     lines.forEach((line) => {
       fileReadHandler(accumulator, line);
     });
-    expect(accumulator).toEqual(['1721', '979', '366', '299', '675', '1456']);
+    expect(accumulator).toEqual([
+      [
+        [2, 4],
+        [6, 8],
+      ],
+      [
+        [2, 3],
+        [4, 5],
+      ],
+      [
+        [5, 7],
+        [7, 9],
+      ],
+      [
+        [2, 8],
+        [3, 7],
+      ],
+      [
+        [6, 6],
+        [4, 6],
+      ],
+      [
+        [2, 6],
+        [4, 8],
+      ],
+    ]);
   });
 });
--- src/day4/file-read-handler.ts
+++ src/day4/file-read-handler.ts
@@ -1,5 +1,10 @@
-const fileReadHandler = (accumulator: Array<string>, line: string): void => {
-  accumulator.push(line);
+const fileReadHandler = (
+  accumulator: Array<Array<Array<number>>>,
+  line: string
+): void => {
+  accumulator.push(
+    line.split(",").map((range) => range.split("-").map((r) => parseInt(r)))
+  );
 };
 
 export default fileReadHandler;
