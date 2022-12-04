ca1d83bb69de2e39df2153565bd4b2a72b4eb5dd
Jonathan Darrer <jonny@jondarrer.me.uk>
2022-12-04 10:23:00 +0000

    4-1 ✨ Add day 4 part 2

--- src/day4/part2.spec.ts
+++ src/day4/part2.spec.ts
@@ -1,14 +1,53 @@
-import part2, { somethingElse } from './part2';
+import part2, { oneRangeOverlapsTheOther } from "./part2";
 
-describe('day4/part2', () => {
-  it('should work out the result', () => {
-    const input = [];
-    expect(part2(input)).toEqual(0);
+describe("day4/part2", () => {
+  it("should work out the result", () => {
+    const input = [
+      [
+        [2, 4],
+        [6, 8],
+      ],
+      [
+        [2, 3],
+        [4, 5],
+      ],
+      [
+        [5, 7],
+        [7, 9],
+      ],
+      [
+        [2, 8],
+        [3, 7],
+      ],
+      [
+        [6, 6],
+        [4, 6],
+      ],
+      [
+        [2, 6],
+        [4, 8],
+      ],
+    ];
+    expect(part2(input)).toEqual(4);
   });
 });
-describe('day4/somethingElse', () => {
-  it('should work out something', () => {
-    const input = 0;
-    expect(somethingElse(input)).toEqual(0);
+describe("day4/oneRangeOverlapsTheOther", () => {
+  it("should work out whether one range overlaps the other: 1", () => {
+    expect(oneRangeOverlapsTheOther([2, 4], [6, 8])).toEqual(false);
+  });
+  it("should work out whether one range overlaps the other: 2", () => {
+    expect(oneRangeOverlapsTheOther([2, 3], [4, 5])).toEqual(false);
+  });
+  it("should work out whether one range overlaps the other: 3", () => {
+    expect(oneRangeOverlapsTheOther([5, 7], [7, 9])).toEqual(true);
+  });
+  it("should work out whether one range overlaps the other: 4", () => {
+    expect(oneRangeOverlapsTheOther([2, 8], [3, 7])).toEqual(true);
+  });
+  it("should work out whether one range overlaps the other: 5", () => {
+    expect(oneRangeOverlapsTheOther([6, 6], [4, 6])).toEqual(true);
+  });
+  it("should work out whether one range overlaps the other: 6", () => {
+    expect(oneRangeOverlapsTheOther([2, 6], [4, 8])).toEqual(true);
   });
 });
--- src/day4/part2.ts
+++ src/day4/part2.ts
@@ -1,6 +1,30 @@
-const part2 = (input: Array<string>): number => 0;
+import { oneRangeContainsTheOther } from "./part1";
 
-const somethingElse = (arg: number): number => 0;
+const part2 = (input: Array<Array<Array<number>>>): number => {
+  let pairedRangesOverlapping = 0;
+
+  for (let i = 0; i < input.length; i++) {
+    if (
+      oneRangeOverlapsTheOther(input[i][0], input[i][1]) ||
+      oneRangeContainsTheOther(input[i][0], input[i][1])
+    ) {
+      pairedRangesOverlapping++;
+    } else {
+      console.log(input[i][0], input[i][1]);
+    }
+  }
+
+  return pairedRangesOverlapping;
+};
+
+const oneRangeOverlapsTheOther = (
+  [aStart, aEnd]: Array<number>,
+  [bStart, bEnd]: Array<number>
+): boolean => {
+  return (
+    (aStart <= bStart && aEnd >= bStart) || (aStart <= bEnd && aEnd >= bEnd)
+  );
+};
 
 export default part2;
-export { somethingElse };
+export { oneRangeOverlapsTheOther };
