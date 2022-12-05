52cf40fca301f012b6e6abcbac2914f932a3cab9
Jonathan Darrer <jonny@jondarrer.me.uk>
2022-12-05 14:21:09 +0000

    5-2 ✨ Add day 5 part 2

--- src/day5/part2.spec.ts
+++ src/day5/part2.spec.ts
@@ -1,14 +1,19 @@
-import part2, { somethingElse } from './part2';
+import part2 from "./part2";
+import { SupplyStacks } from "./supply-stacks";
 
-describe('day5/part2', () => {
-  it('should work out the result', () => {
-    const input = [];
-    expect(part2(input)).toEqual(0);
-  });
-});
-describe('day5/somethingElse', () => {
-  it('should work out something', () => {
-    const input = 0;
-    expect(somethingElse(input)).toEqual(0);
+describe("day5/part2", () => {
+  it("should work out the result", () => {
+    // Arrange
+    const input = new SupplyStacks();
+    input.stacks = [["Z", "N"], ["M", "C", "D"], ["P"]];
+    input.rearrangementProcedures = [
+      [1, 2, 1],
+      [3, 1, 3],
+      [2, 2, 1],
+      [1, 1, 2],
+    ];
+
+    // Act & Assert
+    expect(part2([input])).toEqual("MCD");
   });
 });
--- src/day5/part2.ts
+++ src/day5/part2.ts
@@ -1,6 +1,16 @@
-const part2 = (input: Array<string>): number => 0;
+import { SupplyStacks } from "./supply-stacks";
 
-const somethingElse = (arg: number): number => 0;
+const part2 = ([
+  { stacks, rearrangementProcedures },
+]: Array<SupplyStacks>): string => {
+  for (let i = 0; i < rearrangementProcedures.length; i++) {
+    const [quantity, from, to] = rearrangementProcedures[i];
+    stacks[to - 1].push(
+      ...stacks[from - 1].splice(stacks[from - 1].length - quantity, quantity)
+    );
+  }
+
+  return stacks.flatMap((stack) => stack[stack.length - 1]).join("");
+};
 
 export default part2;
-export { somethingElse };
