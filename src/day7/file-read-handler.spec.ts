5e79c3bb0aa058bdb4e4175a473ba58bbb9652ab
Jonathan Darrer <jonny@jondarrer.me.uk>
2022-12-07 08:06:53 +0000

    7-0 🚧 Implement file reader for day 7

--- src/day7/file-read-handler.spec.ts
+++ src/day7/file-read-handler.spec.ts
@@ -1,12 +1,60 @@
-import fileReadHandler from './file-read-handler';
+import fileReadHandler from "./file-read-handler";
 
-describe('day7/file-read-handler', () => {
-  it('should read in strings and convert them to numbers', () => {
-    const lines = ['1721', '979', '366', '299', '675', '1456'];
+describe("day7/file-read-handler", () => {
+  it("should read in strings and convert them to commands and output", () => {
+    const lines = [
+      "$ cd /",
+      "$ ls",
+      "dir a",
+      "14848514 b.txt",
+      "8504156 c.dat",
+      "dir d",
+      "$ cd a",
+      "$ ls",
+      "dir e",
+      "29116 f",
+      "2557 g",
+      "62596 h.lst",
+      "$ cd e",
+      "$ ls",
+      "584 i",
+      "$ cd ..",
+      "$ cd ..",
+      "$ cd d",
+      "$ ls",
+      "4060174 j",
+      "8033020 d.log",
+      "5626152 d.ext",
+      "7214296 k",
+    ];
     const accumulator = [];
     lines.forEach((line) => {
       fileReadHandler(accumulator, line);
     });
-    expect(accumulator).toEqual(['1721', '979', '366', '299', '675', '1456']);
+    expect(accumulator).toEqual([
+      { type: "cmd", cmd: "cd", args: "/" },
+      { type: "cmd", cmd: "ls", args: "" },
+      { type: "output", fileType: "dir", name: "a", size: undefined },
+      { type: "output", fileType: "file", name: "b.txt", size: 14848514 },
+      { type: "output", fileType: "file", name: "c.dat", size: 8504156 },
+      { type: "output", fileType: "dir", name: "d", size: undefined },
+      { type: "cmd", cmd: "cd", args: "a" },
+      { type: "cmd", cmd: "ls", args: "" },
+      { type: "output", fileType: "dir", name: "e", size: undefined },
+      { type: "output", fileType: "file", name: "f", size: 29116 },
+      { type: "output", fileType: "file", name: "g", size: 2557 },
+      { type: "output", fileType: "file", name: "h.lst", size: 62596 },
+      { type: "cmd", cmd: "cd", args: "e" },
+      { type: "cmd", cmd: "ls", args: "" },
+      { type: "output", fileType: "file", name: "i", size: 584 },
+      { type: "cmd", cmd: "cd", args: ".." },
+      { type: "cmd", cmd: "cd", args: ".." },
+      { type: "cmd", cmd: "cd", args: "d" },
+      { type: "cmd", cmd: "ls", args: "" },
+      { type: "output", fileType: "file", name: "j", size: 4060174 },
+      { type: "output", fileType: "file", name: "d.log", size: 8033020 },
+      { type: "output", fileType: "file", name: "d.ext", size: 5626152 },
+      { type: "output", fileType: "file", name: "k", size: 7214296 },
+    ]);
   });
 });
--- src/day7/file-read-handler.ts
+++ src/day7/file-read-handler.ts
@@ -1,5 +1,21 @@
-const fileReadHandler = (accumulator: Array<string>, line: string): void => {
-  accumulator.push(line);
+const fileReadHandler = (accumulator: Array<object>, line: string): void => {
+  let output;
+  const parts = line.split(" ");
+  if (parts[0] === "$") {
+    output = {
+      type: "cmd",
+      cmd: parts[1],
+      args: parts[2] || "",
+    };
+  } else {
+    output = {
+      type: "output",
+      fileType: parts[0] === "dir" ? "dir" : "file",
+      name: parts[1],
+      size: parts[0] === "dir" ? undefined : parseInt(parts[0]),
+    };
+  }
+  accumulator.push(output);
 };
 
 export default fileReadHandler;
