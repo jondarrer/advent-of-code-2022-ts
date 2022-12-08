10da0e3aca72d4b7c88b9490e83fd5757d9b27dc
Jonathan Darrer <jonny@jondarrer.me.uk>
2022-12-08 01:48:14 +0000

    7-2 ✨ Add day 7 part 2

--- src/day7/part2.spec.ts
+++ src/day7/part2.spec.ts
@@ -1,13 +1,103 @@
-import part2, { somethingElse } from './part2';
+import part2, { somethingElse } from "./part2";
+import { CommandOrOutput, CommandType, FileType } from "./types";
 
-describe('day7/part2', () => {
-  it('should work out the result', () => {
-    const input = [];
-    expect(part2(input)).toEqual(0);
+describe("day7/part2", () => {
+  it("should work out the result", () => {
+    const input: Array<CommandOrOutput> = [
+      { type: "cmd", cmd: CommandType.ChangeDir, args: "/" },
+      { type: "cmd", cmd: CommandType.List, args: "" },
+      {
+        type: "output",
+        fileType: FileType.Dir,
+        name: "a",
+        size: undefined,
+      },
+      {
+        type: "output",
+        fileType: FileType.File,
+        name: "b.txt",
+        size: 14848514,
+      },
+      {
+        type: "output",
+        fileType: FileType.File,
+        name: "c.dat",
+        size: 8504156,
+      },
+      {
+        type: "output",
+        fileType: FileType.Dir,
+        name: "d",
+        size: undefined,
+      },
+      { type: "cmd", cmd: CommandType.ChangeDir, args: "a" },
+      { type: "cmd", cmd: CommandType.List, args: "" },
+      {
+        type: "output",
+        fileType: FileType.Dir,
+        name: "e",
+        size: undefined,
+      },
+      {
+        type: "output",
+        fileType: FileType.File,
+        name: "f",
+        size: 29116,
+      },
+      {
+        type: "output",
+        fileType: FileType.File,
+        name: "g",
+        size: 2557,
+      },
+      {
+        type: "output",
+        fileType: FileType.File,
+        name: "h.lst",
+        size: 62596,
+      },
+      { type: "cmd", cmd: CommandType.ChangeDir, args: "e" },
+      { type: "cmd", cmd: CommandType.List, args: "" },
+      {
+        type: "output",
+        fileType: FileType.File,
+        name: "i",
+        size: 584,
+      },
+      { type: "cmd", cmd: CommandType.ChangeDir, args: ".." },
+      { type: "cmd", cmd: CommandType.ChangeDir, args: ".." },
+      { type: "cmd", cmd: CommandType.ChangeDir, args: "d" },
+      { type: "cmd", cmd: CommandType.List, args: "" },
+      {
+        type: "output",
+        fileType: FileType.File,
+        name: "j",
+        size: 4060174,
+      },
+      {
+        type: "output",
+        fileType: FileType.File,
+        name: "d.log",
+        size: 8033020,
+      },
+      {
+        type: "output",
+        fileType: FileType.File,
+        name: "d.ext",
+        size: 5626152,
+      },
+      {
+        type: "output",
+        fileType: FileType.File,
+        name: "k",
+        size: 7214296,
+      },
+    ];
+    expect(part2(input)).toEqual(24933642);
   });
 });
-describe('day7/somethingElse', () => {
-  it('should work out something', () => {
+describe("day7/somethingElse", () => {
+  it("should work out something", () => {
     const input = 0;
     expect(somethingElse(input)).toEqual(0);
   });
--- src/day7/part2.ts
+++ src/day7/part2.ts
@@ -1,4 +1,26 @@
-const part2 = (input: Array<string>): number => 0;
+import { buildFileSystemCmdsAndOutput, calculateDirSizes } from "./part1";
+import { CommandOrOutput } from "./types";
+
+const part2 = (input: Array<CommandOrOutput>): number => {
+  const fileSystem = buildFileSystemCmdsAndOutput(input);
+  const dirSizes = calculateDirSizes(fileSystem);
+  let remainingSpace: number, minSpaceToFreeUp: number;
+
+  remainingSpace = 70_000_000 - dirSizes.get(""); // Still have a bug wrt root folder name
+  minSpaceToFreeUp = 30_000_000 - remainingSpace;
+
+  const sortedDirsBySize = new Map(
+    [...dirSizes.entries()].sort((a, b) => a[1] - b[1])
+  );
+
+  for (let size of sortedDirsBySize.values()) {
+    if (size >= minSpaceToFreeUp) {
+      return size;
+    }
+  }
+
+  throw new Error("Not found!");
+};
 
 const somethingElse = (arg: number): number => 0;
 
