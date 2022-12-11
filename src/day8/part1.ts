7b29b737af2abef8b93a375b499d102416c92609
Jonathan Darrer <jonny@jondarrer.me.uk>
2022-12-11 08:41:54 +0000

    8-1 ✨ Add day 8 part 1

--- src/day8/part1.ts
+++ src/day8/part1.ts
@@ -1,6 +1,47 @@
-const part1 = (input: Array<string>): number => 0;
+const part1 = (input: Array<Array<number>>): number => {
+  let result = input.length * 4 - 4;
+  let transposed = input[0].map((_, colIndex) =>
+    input.map((row) => row[colIndex])
+  );
 
-const somethingElse = (arg: number): number => 0;
+  for (let i = 1; i < input.length - 1; i++) {
+    for (let j = 1; j < input.length - 1; j++) {
+      if (
+        isTreeVisible(input[i][j], {
+          top: transposed[j].slice(0, i),
+          right: input[i].slice(j + 1),
+          bottom: transposed[j].slice(i + 1),
+          left: input[i].slice(0, j),
+        })
+      ) {
+        result += 1;
+      }
+    }
+  }
+
+  return result;
+};
+
+const isTreeVisible = (
+  treeHeight: number,
+  others: {
+    top: Array<number>;
+    right: Array<number>;
+    bottom: Array<number>;
+    left: Array<number>;
+  }
+): boolean => {
+  const { top, right, bottom, left } = others;
+  return (
+    treeHeight > maxTreeHeight(top) ||
+    treeHeight > maxTreeHeight(right) ||
+    treeHeight > maxTreeHeight(bottom) ||
+    treeHeight > maxTreeHeight(left)
+  );
+};
+
+const maxTreeHeight = (trees: Array<number> = []) =>
+  trees.reduce((prev, curr) => (curr > prev ? curr : prev), -1);
 
 export default part1;
-export { somethingElse };
+export { isTreeVisible };
