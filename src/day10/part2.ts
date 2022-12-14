0f3838935851d98557ae672d35b2478acdf58301
Jonathan Darrer <jdarrer@findmypast.com>
2022-12-14 23:11:07 +0000

    10-2 ✨ Add day 10 part 2

--- src/day10/part2.ts
+++ src/day10/part2.ts
@@ -1,6 +1,38 @@
-const part2 = (input: Array<string>): number => 0;
+import { performOperation } from "./part1";
+import { Command } from "./types";
 
-const somethingElse = (arg: number): number => 0;
+const part2 = (input: Array<Command>): Array<string> => {
+  const result: Array<string> = [];
+  const screen: Array<Array<string>> = [];
+  const registryHistory: Array<number> = [1];
+  const SCREEN_WIDTH = 40;
+
+  for (let i = 0; i < input.length; i++) {
+    performOperation(input[i], registryHistory);
+  }
+
+  for (let i = 0; i < registryHistory.length - 1; i++) {
+    const spriteHorizontalPosition = registryHistory[i];
+    const x = i % SCREEN_WIDTH;
+    const y = Math.floor(i / SCREEN_WIDTH);
+    let pixelState = ".";
+    if (
+      x >= spriteHorizontalPosition - 1 &&
+      x <= spriteHorizontalPosition + 1
+    ) {
+      pixelState = "#";
+    }
+    if (screen[y] === undefined) {
+      screen[y] = [];
+    }
+    screen[y][x] = pixelState;
+  }
+
+  for (let i = 0; i < screen.length; i++) {
+    result.push(screen[i].join(""));
+  }
+
+  return result;
+};
 
 export default part2;
-export { somethingElse };
