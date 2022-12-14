6354d589893f80737c5e748db956c30506171a45
Jonathan Darrer <jonny@jondarrer.me.uk>
2022-12-14 08:53:56 +0000

    9-2 ✨ Add day 9 part 2

--- src/day9/part2.spec.ts
+++ src/day9/part2.spec.ts
@@ -1,14 +1,38 @@
-import part2, { somethingElse } from './part2';
+import part2, { moveTails } from "./part2";
+import { Direction, GridPosition, Move } from "./types";
 
-describe('day9/part2', () => {
-  it('should work out the result', () => {
-    const input = [];
-    expect(part2(input)).toEqual(0);
+describe("day9/part2", () => {
+  it("should work out the result small", () => {
+    const input: Array<Move> = [
+      { direction: Direction.Right, amount: 4 },
+      { direction: Direction.Up, amount: 4 },
+      { direction: Direction.Left, amount: 3 },
+      { direction: Direction.Down, amount: 1 },
+      { direction: Direction.Right, amount: 4 },
+      { direction: Direction.Down, amount: 1 },
+      { direction: Direction.Left, amount: 5 },
+      { direction: Direction.Right, amount: 2 },
+    ];
+    expect(part2(input)).toEqual(1);
+  });
+
+  it("should work out the result large", () => {
+    const input: Array<Move> = [
+      { direction: Direction.Right, amount: 5 },
+      { direction: Direction.Up, amount: 8 },
+      { direction: Direction.Left, amount: 8 },
+      { direction: Direction.Down, amount: 3 },
+      { direction: Direction.Right, amount: 17 },
+      { direction: Direction.Down, amount: 10 },
+      { direction: Direction.Left, amount: 25 },
+      { direction: Direction.Up, amount: 20 },
+    ];
+    expect(part2(input)).toEqual(36);
   });
 });
-describe('day9/somethingElse', () => {
-  it('should work out something', () => {
-    const input = 0;
-    expect(somethingElse(input)).toEqual(0);
+describe.skip("day9/moveTails", () => {
+  it("should work out something", () => {
+    const knots: Array<GridPosition> = [];
+    expect(moveTails(knots)).toEqual([]);
   });
 });
